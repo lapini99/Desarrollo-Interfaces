b8992a2d66560725bcf727d95c0f0107ef443521
Alessandro Lapini <91556456+alapinilopez@users.noreply.github.com>
2022-10-18 11:10:35 +0200

    Nuevo ejericio de tablas

---
+++ Table/src/table/Table.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package table;
+import javax.swing.table.DefaultTableModel;
+
+/**
+ *
+ * @author alela
+ */
+public class Table {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        NewJFrame obj = new NewJFrame();
+        obj.setVisible(true);
+    }
+    
+}
