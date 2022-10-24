7c7463964859623b67ef4fab4b58d4b783014521
Alessandro Lapini <91556456+alapinilopez@users.noreply.github.com>
2022-10-24 14:32:16 +0200

    Añadir ejercicios

---
+++ UD2_15/src/ud2_15/UD2_15.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ud2_15;
+
+/**
+ *
+ * @author alela
+ */
+public class UD2_15 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        NewJFrame obj = new NewJFrame();
+        obj.setVisible(true);
+    }
+    
+}
