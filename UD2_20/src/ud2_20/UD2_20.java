980833e84463a7e81f503de997e6f0ebe2094bf6
Alessandro Lapini <91556456+alapinilopez@users.noreply.github.com>
2022-10-25 19:36:33 +0200

    Añadir ultimo ejercicio

---
+++ UD2_20/src/ud2_20/UD2_20.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ud2_20;
+
+/**
+ *
+ * @author alela
+ */
+public class UD2_20 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        NewJFrame obj = new NewJFrame();
+        obj.setVisible(true);
+    }
+    
+}
